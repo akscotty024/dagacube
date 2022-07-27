50a6f99fd3c5abdfb03c0a413682a173411d8509
AK47DEV <akscotty024@users.noreply.github.com>
2022-07-27 06:14:40 +0200

    Update ErrorDetails.java
    Minor Changes to the appearance of the code

--- src/main/java/com/dagacube/casino/exception/ErrorDetails.java
+++ src/main/java/com/dagacube/casino/exception/ErrorDetails.java
@@ -3,7 +3,8 @@ package com.dagacube.casino.exception;
 import java.util.Date;
 
 public class ErrorDetails {
-    private Date timestamp;
+   // This class is used as part of error handling it contains the time stamp a special message you set in your controller and the detail thereof.
+	private Date timestamp;
     private String message;
     private String details;
 
