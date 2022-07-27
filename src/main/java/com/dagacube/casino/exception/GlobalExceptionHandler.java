16c91f4b483dba3b7fe855be2c919e8fa713f05f
AK47DEV <akscotty024@users.noreply.github.com>
2022-07-27 02:05:31 +0200

    Update GlobalExceptionHandler.java
    Made changes to accommodate the last 10 transactions

--- src/main/java/com/dagacube/casino/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dagacube/casino/exception/GlobalExceptionHandler.java
@@ -12,13 +12,13 @@ import org.springframework.web.context.request.WebRequest;
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+         ErrorDetails ed = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+         return new ResponseEntity<>(ed, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+        ErrorDetails ed = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(ed, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
