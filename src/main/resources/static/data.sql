c666b15ab2e5b87d1b4663f788f54edbf9864ead
AK47DEV <akscotty024@users.noreply.github.com>
2022-07-27 06:15:30 +0200

    Create data.sql
    Starting next portion of work adding a table with data upon loading the app

---
+++ src/main/resources/static/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO player (id, balance,amount,transactionId,transactionType) VALUES (1, '1000', '10000' , '1' , 'WIN');
+INSERT INTO player (id, balance,amount,transactionId,transactionType) VALUES (2, '2000', '20000' , '2' , 'WAGER');
+INSERT INTO player (id, balance,amount,transactionId,transactionType) VALUES (3, '3000', '30000' , '3' , 'WAGER');
+INSERT INTO player (id, balance,amount,transactionId,transactionType) VALUES (4, '4000', '40000' , '4' , 'WAGER');
+INSERT INTO player (id, balance,amount,transactionId,transactionType) VALUES (5, '5000', '50000' , '5' , 'WIN');
